8261bcb8971c79567fd18977e0a7e098ee72bb13
Jingtian Hu <jt.hu@mail.utoronto.ca>
2026-03-29 19:56:51 -0400

    added protocol and server c files

---
+++ server.h
@@ -0,0 +1,26 @@
+#ifndef _SERVER
+#define _SERVER
+
+#ifndef _PROTOCOL
+#define PROTOCOL
+#include "protocol.h"
+#endif
+
+
+typedef struct {
+    int       fd;
+    char      name[MAX_NAME + 1];
+    Card      hand[DECK_SIZE];
+    int       hand_size;
+    int       connected;
+    int       said_uno;          /* declared UNO while at <=2 cards   */
+    int       uno_vulnerable;    /* hand==1 and hasn't said UNO       */
+    int       has_drawn;         /* drew a card this turn already     */
+    uint8_t   buf[NET_BUF_SIZE];
+    int       buf_len;
+} Player;
+
+
+
+
+#endif
